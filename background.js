f315d0e463a1fb544d1d8dcb23f065f578e28500
monojkhatua <monojkhatua@gmail.com>
2017-05-31 11:54:12 +0530

    Added missing file background.js

---
+++ background.js
@@ -0,0 +1,40 @@
+
+var ts0;
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.message === 'capture') {
+        ts0 = performance.now();
+        console.log("Recieved message at T : " + ts0);
+        chrome.tabs.getSelected(null, (tab) => {
+            chrome.tabs.captureVisibleTab(tab.windowId, { format: 'png' }, (image) => {
+                // image is base64
+
+                var matches = [];
+                var normalizedImage;
+                //TODO:Resolve/reject promise if no match happens
+                crop(image, req.area, req.dpr, false, (cropped) => {
+                    normalizedImage = cropped;
+                    var ts1 = performance.now();
+                    console.log("Time taken for snapshot : " + (ts1 - ts0) + " ms");
+
+                    whiteList.forEach(function (value) {
+                        matches.push(matchTemplate(normalizedImage, value))
+                    });
+
+                   // for (i = 0; i < whiteList.length; i++) {
+                   //      // console.log(whiteList[i], normalizedImage);
+                   //      matches[i] = matchTemplate(normalizedImage, whiteList[i]);
+                   //  }
+
+                    Promise.race(matches).then((site) => {
+                        // console.log("After promise");
+                        matchFound = true;
+                        res({ template_match: "Match found", site: site });
+                    })
+                })
+
+            })
+        })
+    }
+    return true
+})
+
