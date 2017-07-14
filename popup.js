6f4182567555662b062733defc1c96c210561175
Ganesh Varadarajan <ganesh@coriolis.co.in>
2017-07-14 12:41:21 +0530

    eslint popup.js

--- popup.js
+++ popup.js
@@ -9,38 +9,38 @@
         }
         console.log(response);
         if (response.status === "greenflagged") {
-            document.getElementById('kp-remove-from-whitelist').style.display = 'block';
-            document.getElementById('kp-add-to-whitelist').style.display = 'none';
-            //document.getElementsByClassName('optsCurrent')[0].style.display = 'block';
+            document.getElementById("kp-remove-from-whitelist").style.display = "block";
+            document.getElementById("kp-add-to-whitelist").style.display = "none";
+            //document.getElementsByClassName("optsCurrent")[0].style.display = "block";
         } else {
-            document.getElementById('kp-add-to-whitelist').style.display = 'block';
+            document.getElementById("kp-add-to-whitelist").style.display = "block";
         }
-    }
+    };
 
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         curTab = tabs[0];
         console.log("Current tab : ", curTab);
         if (isSpecialTab(curTab.url)) {
-            chrome.runtime.sendMessage({op: "update_info", curtab: curTab, status: tab_status.NA});
+            chrome.runtime.sendMessage({op: "update_info", curtab: curTab, status: "NA"});
         } else {
             chrome.runtime.sendMessage({op: "get_tabinfo", curtab: curTab}, handleTabInfo);
         }
     });
 
-    document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('kp-add-to-whitelist').addEventListener('click', function (e) {
+    document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("kp-add-to-whitelist").addEventListener("click", function (e) {
             var site = stripQueryParams(curTab.url);
-            chrome.runtime.sendMessage({ op: 'addToWhitelist', currentTab: curTab, site: site});
+            chrome.runtime.sendMessage({ op: "addToWhitelist", currentTab: curTab, site: site});
             window.close();
         });
-        document.getElementById('kp-remove-from-whitelist').addEventListener('click', function (e) {
+        document.getElementById("kp-remove-from-whitelist").addEventListener("click", function (e) {
             var site = stripQueryParams(curTab.url);
-            chrome.runtime.sendMessage({ op: 'removeFromWhitelist', site: site});
+            chrome.runtime.sendMessage({ op: "removeFromWhitelist", site: site});
             window.close();
         });
-        document.getElementById('settingsLink').addEventListener('click', function (e) {
+        document.getElementById("settingsLink").addEventListener("click", function (e) {
             chrome.tabs.create({
-                url: chrome.extension.getURL('option.html')
+                url: chrome.extension.getURL("option.html")
             });
             window.close();
         });
