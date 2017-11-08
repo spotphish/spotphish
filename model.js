eb69949ff05c2d6be7f15a4ce9fbeadcdd581b7e
Monoj Khatua <monojkhatua@gmail.com>
2017-11-08 10:39:36 +0530

    Toggle site fix

--- model.js
+++ model.js
@@ -238,7 +238,10 @@ let Sites = {
         const cur = _.find(this.customSites, x => x.name === site.name);
         let out = cur ? _.cloneDeep(cur) : {name: site.name, src: site.src};
         out.disabled = !enable;
-        let protected  = site.protected.map(x => x.disabled = !enable);
+        let protected  = site.protected.map(x => {
+            x.disabled = !enable;
+            return x;
+        });
         out.protected = protected;
         return this.dbCustomSites.put(out)
             .then(x => this.sync());
