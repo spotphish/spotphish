9410e7696144b422910b158643328c9e80d630a0
vijays1 <vijay.sharma@coriolis.co.in>
2020-12-04 17:40:25 +0530

    main.json added to ctphab

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var defaultFeeds = [
         "name": "main",
         // "src": "https://spotphish.github.io/feeds/main/main.json"
         // "src":"http://localhost/Max/main.json"
-        "src":"https://vijay-coriolis.github.io/main.json"
+        "src":"./main.json"
     }
 ];
 
