c33b67de4e90c05c2b67827c15b308af16e1b202
vijays1 <vijay.sharma@coriolis.co.in>
2021-01-11 16:31:03 +0530

    docs updated

---
+++ examples/Classification/Classification.js
@@ -0,0 +1,14 @@
+export default class Classification{
+   predict(img){
+     //Your code starts here..
+      /*
+          return {
+            class: <name of predicted class> (string),
+            confidence: <confidence %> (int),
+            time_taken: <time taken to predict> (int),
+            image: <base64 url of predicted image>
+        };
+
+      */
+  }
+}
